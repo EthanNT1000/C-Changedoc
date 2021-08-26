3bfdf0db59242d5333be5624cf423a7e5bd3d466
EthanNT1000 <40427259@gm.nfu.edu.tw>
2021-08-26 09:35:12 +0800

    Created new function.

--- NT1000Gift/Form1.Designer.cs
+++ NT1000Gift/Form1.Designer.cs
@@ -32,24 +32,26 @@ namespace NT1000Gift
             this.changeButton = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.ChooseDir = new System.Windows.Forms.Button();
-            this.chooseCSV = new System.Windows.Forms.Button();
             this.opencsvDialog = new System.Windows.Forms.OpenFileDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.selectFile = new System.Windows.Forms.Button();
             this.openFile = new System.Windows.Forms.OpenFileDialog();
-            this.labelMessage = new System.Windows.Forms.Label();
-            this.status = new System.Windows.Forms.Label();
+            this.print = new System.Windows.Forms.Button();
+            this.copyFile = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.printDoc = new System.Drawing.Printing.PrintDocument();
+            this.lazybut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // changeButton
             // 
             this.changeButton.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.changeButton.Location = new System.Drawing.Point(182, 189);
+            this.changeButton.Location = new System.Drawing.Point(234, 171);
             this.changeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.changeButton.Name = "changeButton";
-            this.changeButton.Size = new System.Drawing.Size(94, 61);
+            this.changeButton.Size = new System.Drawing.Size(106, 49);
             this.changeButton.TabIndex = 0;
             this.changeButton.Text = "更改";
             this.changeButton.UseVisualStyleBackColor = true;
@@ -57,12 +59,12 @@ namespace NT1000Gift
             // 
             // folderBrowserDialog1
             // 
-            this.folderBrowserDialog1.Description = "請選擇所在資料夾";
+            this.folderBrowserDialog1.Description = "請選擇輸出資料夾";
             // 
             // ChooseDir
             // 
             this.ChooseDir.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.ChooseDir.Location = new System.Drawing.Point(12, 86);
+            this.ChooseDir.Location = new System.Drawing.Point(69, 87);
             this.ChooseDir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ChooseDir.Name = "ChooseDir";
             this.ChooseDir.Size = new System.Drawing.Size(133, 51);
@@ -70,17 +72,6 @@ namespace NT1000Gift
             this.ChooseDir.Text = "選擇資料夾";
             this.ChooseDir.UseVisualStyleBackColor = true;
             this.ChooseDir.Click += new System.EventHandler(this.ChooseDir_Click);
-            // 
-            // chooseCSV
-            // 
-            this.chooseCSV.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.chooseCSV.Location = new System.Drawing.Point(12, 190);
-            this.chooseCSV.Name = "chooseCSV";
-            this.chooseCSV.Size = new System.Drawing.Size(144, 61);
-            this.chooseCSV.TabIndex = 4;
-            this.chooseCSV.Text = "選擇更改列表(.csv)";
-            this.chooseCSV.UseVisualStyleBackColor = true;
-            this.chooseCSV.Click += new System.EventHandler(this.chooseCSV_Click);
             // 
             // opencsvDialog
             // 
@@ -95,7 +86,7 @@ namespace NT1000Gift
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Location = new System.Drawing.Point(14, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 20);
             this.label1.TabIndex = 5;
@@ -108,56 +99,76 @@ namespace NT1000Gift
             this.comboBox1.Items.AddRange(new object[] {
             "依序更改資料夾內容",
             "單一檔案依序更改"});
-            this.comboBox1.Location = new System.Drawing.Point(147, 19);
+            this.comboBox1.Location = new System.Drawing.Point(133, 29);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(249, 28);
             this.comboBox1.TabIndex = 6;
             // 
             // selectFile
             // 
-            this.selectFile.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.selectFile.Location = new System.Drawing.Point(160, 86);
+            this.selectFile.Font = new System.Drawing.Font("標楷體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.selectFile.Location = new System.Drawing.Point(69, 171);
             this.selectFile.Name = "selectFile";
             this.selectFile.Size = new System.Drawing.Size(131, 49);
             this.selectFile.TabIndex = 7;
-            this.selectFile.Text = "選擇檔案";
+            this.selectFile.Text = "選擇更改檔案";
             this.selectFile.UseVisualStyleBackColor = true;
             this.selectFile.Click += new System.EventHandler(this.selectFile_Click);
             // 
             // openFile
             // 
             this.openFile.FileName = "openFile";
+            this.openFile.Title = "請選擇更改檔案";
             // 
-            // labelMessage
+            // print
             // 
-            this.labelMessage.AutoSize = true;
-            this.labelMessage.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.labelMessage.Location = new System.Drawing.Point(330, 100);
-            this.labelMessage.Name = "labelMessage";
-            this.labelMessage.Size = new System.Drawing.Size(0, 20);
-            this.labelMessage.TabIndex = 8;
+            this.print.Font = new System.Drawing.Font("標楷體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.print.Location = new System.Drawing.Point(387, 124);
+            this.print.Name = "print";
+            this.print.Size = new System.Drawing.Size(98, 64);
+            this.print.TabIndex = 1;
+            this.print.Text = "列印";
+            this.print.UseVisualStyleBackColor = true;
+            this.print.Click += new System.EventHandler(this.print_Click);
             // 
-            // status
+            // copyFile
             // 
-            this.status.AutoSize = true;
-            this.status.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.status.Location = new System.Drawing.Point(314, 209);
-            this.status.Name = "status";
-            this.status.Size = new System.Drawing.Size(0, 20);
-            this.status.TabIndex = 9;
+            this.copyFile.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.copyFile.Location = new System.Drawing.Point(234, 87);
+            this.copyFile.Name = "copyFile";
+            this.copyFile.Size = new System.Drawing.Size(106, 51);
+            this.copyFile.TabIndex = 0;
+            this.copyFile.Text = "複製資料";
+            this.copyFile.UseVisualStyleBackColor = true;
+            this.copyFile.Click += new System.EventHandler(this.copyFile_Click);
+            // 
+            // folderBrowserDialog2
+            // 
+            this.folderBrowserDialog2.Description = "選擇檔案複製資料夾";
+            // 
+            // lazybut
+            // 
+            this.lazybut.Font = new System.Drawing.Font("標楷體", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lazybut.Location = new System.Drawing.Point(443, 32);
+            this.lazybut.Name = "lazybut";
+            this.lazybut.Size = new System.Drawing.Size(143, 62);
+            this.lazybut.TabIndex = 8;
+            this.lazybut.Text = "製作交車資料";
+            this.lazybut.UseVisualStyleBackColor = true;
+            this.lazybut.Click += new System.EventHandler(this.lazybut_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 292);
-            this.Controls.Add(this.status);
-            this.Controls.Add(this.labelMessage);
+            this.ClientSize = new System.Drawing.Size(624, 278);
+            this.Controls.Add(this.lazybut);
+            this.Controls.Add(this.print);
+            this.Controls.Add(this.copyFile);
+            this.Controls.Add(this.ChooseDir);
             this.Controls.Add(this.selectFile);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.chooseCSV);
-            this.Controls.Add(this.ChooseDir);
             this.Controls.Add(this.changeButton);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
@@ -173,15 +184,17 @@ namespace NT1000Gift
         private System.Windows.Forms.Button changeButton;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button ChooseDir;
-        private System.Windows.Forms.Button chooseCSV;
         private System.Windows.Forms.OpenFileDialog opencsvDialog;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button selectFile;
         private System.Windows.Forms.OpenFileDialog openFile;
-        private System.Windows.Forms.Label labelMessage;
-        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Button print;
+        private System.Windows.Forms.Button copyFile;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Drawing.Printing.PrintDocument printDoc;
+        private System.Windows.Forms.Button lazybut;
     }
 }
 
--- NT1000Gift/Form1.cs
+++ NT1000Gift/Form1.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Windows.Forms;
 using System.IO;
 using Spire.Doc;
+using System.Printing;
+using System.Diagnostics;
+using System.Threading;
+using Spire.Pdf;
 
 namespace NT1000Gift
 {
@@ -14,6 +18,7 @@ namespace NT1000Gift
         public string[] files;
         public string csv;
         public string filePath;
+        public string[] BusFiles;
 
         public Form1()
         {
@@ -28,73 +33,166 @@ namespace NT1000Gift
         
        
         private void changeButton_Click(object sender, EventArgs e)
-        {            
-            IEnumerable lines = File.ReadLines(csv, System.Text.Encoding.GetEncoding(950));
-            int L = 0;
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-            string[] keys = new string[0];
-            bool Status = true;
-            foreach (string line in lines)
-            {                    
-                if(L == 0)
+        {
+            if (opencsvDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                csv = opencsvDialog.FileName;
+                try
                 {
-                    keys = line.Split(',');                   
-                }
-              
-                if (L > 0)
-                {
-                    string[] values = line.Split(',');
-                    for(int i =0;i<keys.Length;i++)
+                    IEnumerable lines = File.ReadLines(csv, System.Text.Encoding.GetEncoding(950));
+                    int L = 0;
+                    Dictionary<string, string> dict = new Dictionary<string, string>();
+                    string[] keys = new string[0];
+                    bool Status = true;
+                    foreach (string line in lines)
                     {
-                        dict[keys[i]] = values[i];                     
-                    }
-                    
-                    Document doc = new Document();
-                    if (comboBox1.SelectedIndex == 0)
-                    {
-                        try { doc.LoadFromFile(files[L - 1]); }
-                        catch
+                        if (L == 0)
                         {
-                            labelMessage.Text = "資料夾文件數量與更改列表不符:"+ files.Length.ToString();
-                            System.Windows.MessageBox.Show(L.ToString());
-                            Status = false;
+                            keys = line.Split(',');
                         }
-                    }
-                    else if (comboBox1.SelectedIndex == 1)
-                    {
-                        try { doc.LoadFromFile(filePath); }
-                        catch
-                        {
-                            labelMessage.Text = "未選擇更改文件";
-                            Status = false;
-                        }
-                    }
-                    if (Status)
-                    {
-                        try
-                        {
-                            foreach (string key in dict.Keys)
-                            {
-                                doc.Replace(key, dict[key], true, true);
-                            }
-                            doc.SaveToFile(folderBrowserDialog1.SelectedPath + '\\' + L.ToString()+"_"+ dict.Values.First() + ".docx", FileFormat.Docx2013);
 
-                            status.Text = "更改成功";
-                            doc.Close();
-                        }
-                        catch
+                        if (L > 0)
                         {
-                            status.Text = "更改失敗";
+                            string[] values = line.Split(',');
+                            for (int i = 0; i < keys.Length; i++)
+                            {
+                                dict[keys[i]] = values[i];
+                            }
+
+                            Document doc = new Document();
+                            if (comboBox1.SelectedIndex == 0)
+                            {
+                                try { doc.LoadFromFile(files[L - 1]); }
+                                catch
+                                {
+                                    System.Windows.MessageBox.Show("資料夾文件數量與更改列表不符:" + files.Length.ToString());
+                                    Status = false;
+                                }
+                            }
+                            else if (comboBox1.SelectedIndex == 1)
+                            {
+                                try { doc.LoadFromFile(filePath); }
+                                catch
+                                {
+                                    System.Windows.MessageBox.Show("未選擇更改文件");
+                                    Status = false;
+                                }
+                            }
+                            else
+                            {
+                                System.Windows.MessageBox.Show("未選擇模式");
+                                Status = false;
+                                break;
+                            }
+                            if (Status)
+                            {
+                                try
+                                {
+                                    foreach (string key in dict.Keys)
+                                    {
+                                        doc.Replace(key, dict[key], true, true);
+                                    }
+                                    doc.SaveToFile(folderBrowserDialog1.SelectedPath + '\\' + L.ToString() + "_" + dict.Values.First() + ".docx", Spire.Doc.FileFormat.Docx2013);
+                                    doc.Close();
+                                }
+                                catch
+                                {
+                                    System.Windows.MessageBox.Show("更改失敗");
+                                }
+                            }
                         }
-                    }                   
+                        L += 1;
+                    }
+                    L = 0;
+                    if (Status) System.Windows.MessageBox.Show("更改成功");
                 }
-                L += 1;
-            }
-            L = 0;
+                catch { System.Windows.MessageBox.Show("未選擇CSV檔"); }
+            }          
         }
 
 
         private void ChooseDir_Click(object sender, EventArgs e)
+        {
+            
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(folderBrowserDialog1.SelectedPath))
+                {
+                    System.Windows.MessageBox.Show("資料夾路徑不能為空", "提示");
+                    
+                    return;
+                }
+                files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            }           
+        }
+
+        private void selectFile_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = openFile.FileName;
+            }
+        }
+
+        private void copyFile_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(folderBrowserDialog2.SelectedPath))
+                {
+                    System.Windows.MessageBox.Show("資料夾路徑不能為空", "提示");
+                    return;
+                }
+                try
+                {
+                    BusFiles = Directory.GetFiles(folderBrowserDialog2.SelectedPath);
+                    foreach (string f in BusFiles)
+                    {
+                        string newf = f.Replace(folderBrowserDialog2.SelectedPath, "");
+                        File.Copy(f, folderBrowserDialog1.SelectedPath + '\\' + newf, true);
+                    }
+                    System.Windows.MessageBox.Show("複製成功");
+                }
+                catch
+                {
+                    System.Windows.MessageBox.Show("未選擇輸出資料夾");
+                }
+            }
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            string[] dirs = Directory.GetDirectories(folderBrowserDialog1.SelectedPath);
+            foreach (string dir in dirs)
+            {
+                string[] files = Directory.GetFiles(dir);
+                foreach (string file in files)               
+                {
+                    if (file.Contains(".pdf"))
+                    {
+                        PdfDocument pdf = new PdfDocument();
+                        pdf.LoadFromFile(file);
+                        pdf.PrintSettings.Color = false;
+                        pdf.PrintSettings.DocumentName = file.Replace(dir+'\\',"");
+                        pdf.Print();
+                        pdf.Close();
+                    }
+
+                    else
+                    {
+                        Document doc = new Document();
+                        doc.LoadFromFile(file);
+                        printDoc = doc.PrintDocument;
+                        printDoc.DocumentName = file.Replace(dir+'\\', ""); ;
+                        printDoc.Print();
+                        doc.Close();
+                    }
+                }
+            }
+
+        }
+
+        private void lazybut_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
@@ -104,23 +202,84 @@ namespace NT1000Gift
                     return;
                 }
             }
-            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
-        }
 
-        private void chooseCSV_Click(object sender, EventArgs e)
-        {
-            if (opencsvDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                csv = opencsvDialog.FileName;
-            }
-        }
-
-        private void selectFile_Click(object sender, EventArgs e)
-        {
             if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 filePath = openFile.FileName;
             }
+
+            if (opencsvDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                csv = opencsvDialog.FileName;
+            }
+
+            if (folderBrowserDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(folderBrowserDialog2.SelectedPath))
+                {
+                    System.Windows.MessageBox.Show("資料夾路徑不能為空", "提示");
+                    return;
+                }
+                BusFiles = Directory.GetFiles(folderBrowserDialog2.SelectedPath);
+            }
+
+            IEnumerable lines = File.ReadLines(csv, System.Text.Encoding.GetEncoding(950));
+            int L = 0;
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            string[] keys = new string[0];
+            bool Status = true;
+            foreach (string line in lines)
+            {
+                if (L == 0)
+                {
+                    keys = line.Split(',');
+                }
+
+                if (L > 0)
+                {
+                    string[] values = line.Split(',');
+                    for (int i = 0; i < keys.Length; i++)
+                    {
+                        dict[keys[i]] = values[i];
+                    }
+
+                    Document doc = new Document();
+                    try { doc.LoadFromFile(filePath); }
+                    catch
+                    {
+                        System.Windows.MessageBox.Show("未選擇更改文件");
+                        Status = false;
+                    }
+
+                    if (Status)
+                    {
+                        try
+                        {
+                            foreach (string key in dict.Keys)
+                            {
+                                doc.Replace(key, dict[key], true, true);
+                            }
+                            string dir = folderBrowserDialog1.SelectedPath + '\\' + dict.Values.First();
+                            Directory.CreateDirectory(dir);
+                            doc.SaveToFile(dir + '\\' + "01-05_" + dict.Values.First() + ".docx", Spire.Doc.FileFormat.Docx2013);                           
+                            doc.Close();
+
+                            foreach (string f in BusFiles)
+                            {
+                                string newf = f.Replace(folderBrowserDialog2.SelectedPath, "");
+                                File.Copy(f, dir + '\\' + newf, true);
+                            }
+
+                        }
+                        catch
+                        {
+                            System.Windows.MessageBox.Show("複製失敗");
+                        }
+                    }
+                }
+                L += 1;
+            }
+            if (Status) System.Windows.MessageBox.Show("更改成功");
         }
     }
 }
